daea831641b5090036b953f509c3ebb375a44518
izhdenevav <vizdeneva@gmail.com>
2025-11-11 10:45:33 +0700

    add 2 task

---
+++ ex02/learning_tf2_py/launch/turtle_tf2_dynamic_frame_demo_launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    radius_arg = DeclareLaunchArgument(
+        'radius', default_value='2.0',
+        description='Радиус орбиты морковки вокруг turtle1'
+    )
+    direction_arg = DeclareLaunchArgument(
+        'direction_of_rotation', default_value='1',
+        description='1 - по часовой, -1 - против часовой'
+    )
+
+    return LaunchDescription([
+        radius_arg,
+        direction_arg,
+
+        IncludeLaunchDescription(
+            PathJoinSubstitution([
+                FindPackageShare('learning_tf2_py'), 'launch', 'turtle_tf2_demo_launch.py']),
+            launch_arguments={'target_frame': 'carrot1'}.items(),
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='dynamic_frame_tf2_broadcaster',
+            name='dynamic_broadcaster',
+            parameters=[
+                {'radius': LaunchConfiguration('radius')},
+                {'direction_of_rotation': LaunchConfiguration('direction_of_rotation')}
+            ]
+        ),
+    ])
